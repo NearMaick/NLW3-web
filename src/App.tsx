a0b9f0d4da434c6511fc617452cd932d44861e39
Maick Souza <maick_a_s@msn.com>
2020-11-08 17:04:24 -0300

    feat(web): making landing page #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
 import React from 'react';
 
+import './styles/global.css';
+import './styles/pages/landing.css';
+
+import logoImg from './images/logo.svg';
+
 const App: React.FC = () => {
-  // Parei aqui: 58:00
-  return <h1>Maick Souza</h1>;
+  // Parei aqui: 1:12'59
+  return (
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy" />
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Guarulhos</strong>
+          <span>São Paulo</span>
+        </div>
+
+        <a href="#none" className="enter-app">
+          {'>'}
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default App;
