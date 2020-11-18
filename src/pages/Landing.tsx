3a3fd40e662fa56e0dde8ba03b142d3ad2c7e1eb
Maick Souza <maick_a_s@msn.com>
2020-11-18 18:09:59 -0300

    feat(web): setting routes #1

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { FiArrowRight } from 'react-icons/fi';
+
+import '../styles/pages/landing.css';
+
+import logoImg from '../images/logo.svg';
+
+const Landing: React.FC = () => {
+  return (
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy" />
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Guarulhos</strong>
+          <span>São Paulo</span>
+        </div>
+
+        <a href="#none" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
